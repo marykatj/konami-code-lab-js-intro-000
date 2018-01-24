ea02e4b884e58c8661414e99777ef3df9b547975
Mary-Kate James <marykatj@gmail.com>
2018-01-24 18:38:11 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,10 +2,10 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init(e) {
   let index = 0;
-  console.log(e.detail || e.which);
   const key = document.getElementByTagName('script');
     key.addEventListener('keydown', function(e)) {
-
+      console.log(e.detail || e.which);
+      
         if (key === code[index]) {
         index++;
 
@@ -38,4 +38,23 @@ function init(e) {
 }
   for (let i = 0; 1 = key.length; i < 1; i++) {
 const key = document.getElementByTagName('script')[0];
+
+function init(e) {
+  let index = 0;
+  console.log(e.detail || e.which);
+  const key = document.getElementByTagName('script');
+    key.addEventListener('keydown', function(e)) {
+
+        if (key === code[index]) {
+        index++;
+
+          if (index === code.length) {
+          return alert("Hurray!");
+
+          index = 0;
+      } else {
+      index = 0;
+    }
+  }
+}
 */
