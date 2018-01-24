d411c1e223ee2eea967df8b42228da75a6bcbaa3
Mary-Kate James <marykatj@gmail.com>
2018-01-24 18:39:12 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,9 +3,9 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 function init(e) {
   let index = 0;
   const key = document.getElementByTagName('script');
-    key.addEventListener('keydown', function(e)) {
-      console.log(e.detail || e.which);
-      
+    key.addEventListener('keydown', function(e) {
+      console.log(e.detail || e.which)}) {
+
         if (key === code[index]) {
         index++;
 
